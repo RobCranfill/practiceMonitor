af5cdb79b32ae79fd60db9b70008a5e9602d3fe0
Rob Cranfill <robcranfill@gmail.com>
2023-11-03 15:38:05 -0700

    Remove old files

--- gb.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-# send MIDI data thru Python program
-
-amidi -d -p hw:0,0,0 | python3 b.py
-
--- gc.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-# send MIDI data thru Python program
-
-HW_ADDR=`amidi -l  | grep MPKmini2 | awk '{print $2}'`
-
-amidi -d -p $HW_ADDR | python3 c.py
-
