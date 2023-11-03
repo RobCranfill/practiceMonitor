0200ffb217f58073726f0eab5292c4caede0bf6a
Rob Cranfill <robcranfill@gmail.com>
2023-11-02 19:42:33 -0700

    Integrate display

--- PracticeDisplay.py
+++ PracticeDisplay.py
@@ -1,7 +1,6 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# -*- coding: utf-8 -*-
+# PracticeDisplay object
+# robcranfill
+# Based on Adafruit code by ladyada
 
 import time
 import subprocess
@@ -10,22 +9,23 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7789
 
+
 class PracticeDisplay:
 
     def __init__(self):
 
-        # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+        # Configuration for CS and DC pins
         cs_pin = digitalio.DigitalInOut(board.CE0)
         dc_pin = digitalio.DigitalInOut(board.D25)
         reset_pin = None
 
-        # Config for display baudrate (default max is 24mhz):
+        # Config for display baudrate (default max is 24mhz)
         BAUDRATE = 64000000
 
-        # Setup SPI bus using hardware SPI:
+        # Setup SPI bus using hardware SPI
         spi = board.SPI()
 
-        # Create the ST7789 display:
+        # Create the ST7789 display
         disp = st7789.ST7789(
             spi,
             cs=cs_pin,
@@ -89,20 +89,20 @@ class PracticeDisplay:
         self.disp_.image(self.image_, self.rotation_)
 
     def draw_line_color(self, line_number, string, color):
-        x =  0
+        x = 0
         y = line_number * 24
+        w = self.width_
         h = 24
-        w = self.font_.getlength(string)
 
         # black out old text
-        # print("clearing {x}, {y}, {h}, {w}")
-        self.draw_.rectangle((0, 0, w, h), outline=0, fill=0)
+        # print(f"clearing {x}, {y}, {w}, {y+h}")
+        self.draw_.rectangle((0, y, w, y+h), outline=0, fill="#000000")
 
         self.draw_.text((x, y), string, font=self.font_, fill=color)
         self.disp_.image(self.image_, self.rotation_)
 
-    def draw_line_black(self, line_number, string):
-        draw_line_color(self, line_number, string, "#FFFFFF")
+    def draw_line_white(self, line_number, string):
+        self.draw_line_color(line_number, string, "#FFFFFF")
 
 def test():
     pd = PracticeDisplay()
@@ -110,6 +110,7 @@ def test():
     pd.draw_line_color(1, "Hey Cran!",       "#FF0000")
     pd.draw_line_color(2, "   what's",       "#00FF00")
     pd.draw_line_color(3, "     happening?", "#00FFFF")
+    pd.draw_line_white(4, " okeedokee??")
     
     print("displaying????")
     # while True:
