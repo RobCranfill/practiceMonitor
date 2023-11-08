5db3a710d615417944d491db04631bfcebfb1320
Rob Cranfill <robcranfill@gmail.com>
2023-11-08 09:46:33 -0800

    Added my code to even loop

--- tk_a.py
+++ tk_a.py
@@ -1,24 +1,39 @@
 # first TK experiments
 
 from tkinter import *
-from tkinter import ttk
 
-root_widget = Tk()
-frame = ttk.Frame(root_widget, padding=10, name="cran")
-frame.grid()
+BG_COLOR = "blue"
+MIDI_EVENT_DELAY_MS = 1000
 
-# ttk.Label(frame, text="Hello Cran!").grid(column=0, row=0)
 
-label = ttk.Label(
-    frame,
-    text="Hello, Cranface!",
-    foreground="white",  # Set the text color to white
-    background="black"   # Set the background color to black
-    )
-label.grid(column=0, row=0)
+def build_gui():
+    root = Tk()  # create a root widget
+    root.title("Practice Monitor v 0.1")
+    root.configure(bg=BG_COLOR)
+    root.minsize(1400, 1000)  # width, height
+    root.maxsize(1400, 1000)
+    root.geometry("1400x1000+200+100")  # width x height + x + y
 
-ttk.Button(frame, text="Quit", command=root_widget.destroy).grid(column=1, row=1)
 
-root_widget.mainloop()
+    label_time_total = Label(root, text="00:00:00", font=("Helvetica", 72), bg=BG_COLOR, fg="white")
+    label_time_total.pack()
+
+    label_sessions = Label(root, text="Sessions: 0", font=("Helvetica", 32), bg=BG_COLOR, fg="white")
+    label_sessions.pack()
+
+    return root
+
+
+def check_midi(app):
+    print("check_midi")
+    app.after(MIDI_EVENT_DELAY_MS, check_midi, app)  # reschedule event in 2 seconds
+
+
+app_window = build_gui()
+app_window.after(MIDI_EVENT_DELAY_MS, check_midi, app_window)
+
+
+app_window.mainloop()
+
 
 
