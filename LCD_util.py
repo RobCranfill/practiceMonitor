2745829a46281c440ba122154d289a71e96b4907
Rob Cranfill <robcranfill@gmail.com>
2023-11-29 09:02:56 -0800

    added lcd util

---
+++ LCD_util.py
@@ -0,0 +1,32 @@
+# For the Adafruit miniPiTFT 3.3" LCD display, turn on/off (mostly off) the backlight
+# robcranfill
+
+import board
+import digitalio
+import sys
+
+def set_backlight_state(toOn):
+    backlight_ = digitalio.DigitalInOut(board.D22)
+    backlight_.switch_to_output()
+    backlight_.value = toOn
+
+if __name__ == "__main__":
+
+    yeses = ["on", "yes", "1"]
+    nos   = ["off", "no", "0"]
+    usage = str(yeses) + " | " + str(nos)
+    
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} {usage}")
+        sys.exit(1)
+
+    if sys.argv[1].lower() in yeses:
+        set_backlight_state(True)
+        sys.exit(0)
+
+    if sys.argv[1].lower() in nos:
+        set_backlight_state(False)
+        sys.exit(0)
+
+    print(f"Usage: {sys.argv[0]} {usage}")
+    sys.exit(1)
