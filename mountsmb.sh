131794d60213c3cd32371b699a1bc2bac718c993
Rob Cranfill <robcranfill@gmail.com>
2024-01-17 16:22:12 -0800

    doc script

--- mountsmb.sh
+++ mountsmb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # mount the pi via samba so I can work on it with VSCode
+# this runs on the laptop/desktop, not the Pi Zero
 
 PWVAR=`cat pw.text`
 sudo mount -t cifs -o username=rob,password=$PWVAR,uid=1000,rw //pizero2w.local/home /mnt/pizero2w
